aae4dcecbc41ceb34071e1375c5836f03cfa83e3
sates298 <stanislaw.wo@interia.pl>
2019-10-22 14:04:12 +0200

    finish kmp matcher

--- jftt/1list/src/pl/swozniak/Main.java
+++ jftt/1list/src/pl/swozniak/Main.java
@@ -2,7 +2,13 @@ package pl.swozniak;
 
 public class Main {
 
+    private static String text = "łąłąłąłą";
+    private static String pattern = "łą";
+
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("text = " + text);
+        System.out.println("pattern = " + pattern);
+        KnuthMorrisPratt kmp = new KnuthMorrisPratt();
+        System.out.println("Knuth-Morris-Pratt result = " + kmp.kmpMatcher(text, pattern));
     }
 }
