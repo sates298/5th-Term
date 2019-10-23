676d69ebe190a9a460214cf91a61346aef32c830
sates298 <stanislaw.wo@interia.pl>
2019-10-23 14:26:34 +0200

    start finite automata

---
+++ jftt/1list/src/pl/swozniak/FiniteAutomaton.java
@@ -0,0 +1,13 @@
+package pl.swozniak;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FiniteAutomaton {
+    public List<Integer> faMatcher(String text, String pattern) {
+        List<Integer> result = new ArrayList<Integer>();
+        return result;
+    }
+
+
+}
