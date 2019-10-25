1fcd5e5a20aa6eafe2ed7bd0a3741e868dc2c9e0
sates298 <stanislaw.wo@interia.pl>
2019-10-25 22:02:19 +0200

    finish finite automaton matcher

--- jftt/1list/src/pl/swozniak/FiniteAutomaton.java
+++ jftt/1list/src/pl/swozniak/FiniteAutomaton.java
@@ -1,11 +1,66 @@
 package pl.swozniak;
 
-import java.util.ArrayList;
-import java.util.List;
+ //todo change library
+
+import java.util.*;
 
 public class FiniteAutomaton {
+
+
+
+    private boolean isSuffix (String full, String ending){
+        int fl = full.length(), el = ending.length();
+        if (fl >= el){
+            return full.substring(fl-el, fl).compareTo(ending) == 0;
+        }
+        return false;
+    }
+
+    private Map<Pair<Integer, Character>, Integer> computeTransitionFunction(String pattern, String alphabet) {
+        int m = pattern.length();
+        Map<Pair<Integer, Character>, Integer> delta = new HashMap<Pair<Integer, Character>, Integer>();
+
+        for (int q = 0; q < m + 1; q++) {
+            for (char c : alphabet.toCharArray()){
+                int k = Math.min(m+1, q+2);
+
+                do{
+                    k--;
+                }while (!isSuffix(pattern.substring(0, q) + c, pattern.substring(0, k)));
+
+                delta.put(Pair.of(q, c), k);
+            }
+        }
+
+
+        return delta;
+    }
+
+    private String getAlphabet(String pattern){
+        Set<Character> alphabet = new HashSet<Character>();
+        for(Character c: pattern.toCharArray()){
+            alphabet.add(c);
+        }
+
+        StringBuilder builder = new StringBuilder();
+        for(Character c : alphabet){
+            builder.append(c);
+        }
+        return builder.toString();
+    }
+
     public List<Integer> faMatcher(String text, String pattern) {
         List<Integer> result = new ArrayList<Integer>();
+        int n = text.length(), m = pattern.length(), q = 0, s;
+        Map<Pair<Integer, Character>, Integer> delta = computeTransitionFunction(pattern, getAlphabet(pattern));
+        for (int i = 0; i < n; i++){
+            q = delta.get(Pair.of(q, text.charAt(i)));
+
+            if (q == m){
+                s = i - m + 1;
+                result.add(s);
+            }
+        }
         return result;
     }
 
--- jftt/1list/src/pl/swozniak/Main.java
+++ jftt/1list/src/pl/swozniak/Main.java
@@ -2,13 +2,15 @@ package pl.swozniak;
 
 public class Main {
 
-    private static String text = "łąłąłąłą";
-    private static String pattern = "łą";
+    private static String text = "bababaaababa";
+    private static String pattern = "aba";
 
     public static void main(String[] args) {
         System.out.println("text = " + text);
         System.out.println("pattern = " + pattern);
+        FiniteAutomaton fa = new FiniteAutomaton();
+        System.out.println("Finite Automaton Matcher result = " + fa.faMatcher(text, pattern));
         KnuthMorrisPratt kmp = new KnuthMorrisPratt();
-        System.out.println("Knuth-Morris-Pratt result = " + kmp.kmpMatcher(text, pattern));
+        System.out.println("Knuth-Morris-Pratt Matcher result = " + kmp.kmpMatcher(text, pattern));
     }
 }
