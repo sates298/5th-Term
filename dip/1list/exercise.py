b52899caeede981221eff5eef6aa11913c99f14e
sates298 <stanislaw.wo@interia.pl>
2019-10-30 10:51:00 +0100

    finish first programm

--- dip/1list/exercise.py
+++ dip/1list/exercise.py
@@ -12,14 +12,60 @@ gamma = 3
 converted = np.power(gray, gamma)
 
 # gaussian filtering
-# cv.getGaussianKernel(ksize=3, sigma=10)
-blur = cv.GaussianBlur(converted, (7,7), 3)
+blur = cv.GaussianBlur(converted, (19,19), 6)
+
+# check neighbour pixels
+max = 0
+curr = 0
+width = len(blur)
+height = len(blur[0])
+for i in range(width):
+    for j in range(height):
+        if i-1 >=0:
+            if j-1 >=0:
+                curr = abs(1 - (blur[i-1][j-1] / blur[i][j]))
+                if curr > max:
+                    max = curr
+            if j+1 < height:
+                curr = abs(1 - (blur[i-1][j+1] / blur[i][j]))
+                if curr > max:
+                    max = curr
+            curr = abs(1 - (blur[i-1][j] / blur[i][j]))
+            if curr > max:
+                max = curr
+        if i+1 < width:
+            if j-1 >=0:
+                curr = abs(1 - (blur[i+1][j-1] / blur[i][j]))
+                if curr > max:
+                    max = curr
+            if j+1 < height:
+                curr = abs(1 - (blur[i+1][j+1] / blur[i][j]))
+                if curr > max:
+                    max = curr
+            curr = abs(1 - (blur[i+1][j] / blur[i][j]))
+            if curr > max:
+                max = curr
+        if j-1 >=0:
+            curr = abs(1 - (blur[i][j-1] / blur[i][j]))
+            if curr > max:
+                max = curr
+        if j+1 < height:
+            curr = abs(1 - (blur[i][j+1] / blur[i][j]))
+            if curr > max:
+                max = curr
 
 
+# print(max)
 cv.imshow("original", original)
+
 cv.imshow("grayscale", gray)
+cv.imwrite("grayscale.jpg", gray)
+
 cv.imshow("gamma", converted)
+cv.imwrite("gamma_conversion.jpg", converted)
+
 cv.imshow("gaussian", blur)
+cv.imwrite("gaussian_filtering.jpg", gray)
 
 cv.waitKey(100000)
 
