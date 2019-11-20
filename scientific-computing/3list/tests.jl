db3bb87a9d37f628d7a1bdccb6518583b19a525b
sates298 <stanislaw.wo@interia.pl>
2019-11-20 21:55:50 +0100

    add plots and conclusions

--- scientific-computing/3list/tests.jl
+++ scientific-computing/3list/tests.jl
@@ -8,16 +8,20 @@ using Methods
 delta = epsilon = 10^(-5)
 it = 10000
 
-f(x) = Base.MathConstants.e^(1-x) - 1
-g(x) = x * Base.MathConstants.e^(-x)
-f_(x) = -Base.MathConstants.e^(1-x)
-g_(x) = Base.MathConstants.e^(-x) * (1-x)
+f(x) = x^2 + 2*x + 1
+f_(x) = 2*x + 2
+
+a = 0.0
+b = 2.0
+
+x0 = 5.0
+
+x01 = -2.0
+x1 = 4.0
 
 
-println(mbisekcji(f, 0.0, 2.0, delta, epsilon))
-println(mstycznych(f, f_, 5.0, delta, epsilon, it))
-println(msiecznych(f, -2.0, 4.0, delta, epsilon, it))
-println()
-println(mbisekcji(g, -0.5, 1.0, delta, epsilon))
-println(mstycznych(g,g_, 2.0, delta, epsilon, it))
-println(msiecznych(g, -1.0, 0.5, delta, epsilon, it))
+println("bisekcja a=", a, " b=", b, " wynik=",mbisekcji(f, a, b, delta, epsilon))
+
+println("\nnewton x0=", x0, " wynik=",mstycznych(f, f_, x0, delta, epsilon, it))
+
+println("\nsieczne x0=", x01, " x1=", x1, " wynik=",msiecznych(f, x01, x1, delta, epsilon, it))
